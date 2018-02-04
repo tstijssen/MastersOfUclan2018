6065bce092acb1720e53bab17823f9947f5ab31b
TJKendall <tjkendall@uclan.ac.uk>
2018-02-04 20:10:31 +0000

    Pause menu done
    Pause menu completed, lose control when returning to lobby from game.

--- Assets/Scripts/Offline/Game/GameController.cs
+++ Assets/Scripts/Offline/Game/GameController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-    
+    public GameObject selector;
+    private int selectionCount = 0;
 
     public GameObject car;
     public GameObject Tank;
@@ -12,6 +14,7 @@ public class GameController : MonoBehaviour {
     public GameObject InGameMenu;
 
     public bool paused = false;
+    public bool AxisInUse = false;
 
     // Use this for initialization
     void Start ()
@@ -47,6 +50,48 @@ public class GameController : MonoBehaviour {
         {
             Time.timeScale = 0f;
             InGameMenu.SetActive(true);
+
+            float axis = Input.GetAxis("DpadVert");
+
+            if(axis >= -0.4f && axis <= 0.4f)
+            {
+                AxisInUse = false;
+            }
+
+            //Down
+            if (Input.GetAxis("DpadVert") > 0.5f && !AxisInUse && selectionCount < 2)
+            {
+                selector.transform.position -= new Vector3(0f, 30f, 0f);
+                AxisInUse = true;
+                selectionCount++;
+            }
+
+            //Up
+            if (Input.GetAxis("DpadVert") < -0.5f && !AxisInUse && selectionCount > 0)
+            {
+                selector.transform.position += new Vector3(0f, 30f, 0f);
+                AxisInUse = true;
+                selectionCount--;
+            }
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                switch (selectionCount)
+                {
+                    case 0:
+                        paused = false;
+                        break;
+                    case 1:
+                        SceneManager.LoadScene("Battle OFfline");
+                        break;
+                    case 2:
+                        SceneManager.LoadScene("Lobby OFfline");
+                        break;
+                }
+            }
+            
+
+
         }
         else
         {
