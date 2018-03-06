eb6ca07a7dbe73c93538519eec4b18598b7fce3d
unknown <TSTijssen@DEV.LOCAL>
2018-03-06 14:23:24 +0000

    iabdiabdiauhdiagdiaygsdiaugdiaeda

---
+++ Assets/PickupBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupBehaviour : MonoBehaviour {
+
+    public float SpinSpeed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(SpinSpeed * Time.deltaTime,SpinSpeed * Time.deltaTime,0);
+	}
+
+}
---
+++ Assets/Scripts/Interface/GameSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameSettings : MonoBehaviour {
+
+    public GameObject mainMenu;
+
+    public Button apply;
+    public Button cancel;
+
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
