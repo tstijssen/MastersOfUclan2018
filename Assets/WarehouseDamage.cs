cf69cce9138c1d1b6d9826c553c4751813c0f531
TJKendall89 <tjkendall@uclan.ac.uk>
2018-05-03 17:17:17 +0100

    hwalth for warehouse

---
+++ Assets/WarehouseDamage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarehouseDamage : MonoBehaviour {
+    public Slider health;
+    public float hp;
+    public Image hpImage;
+
+	// Use this for initialization
+	void Start ()
+    {
+       // hp = health.maxValue;
+        //health.value = hp;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //health.value = hp;
+        hpImage.fillAmount = hp;
+        //Debug.Log(hp);
+        //Debug.Log(health.value);
+        if(hp < 0f)
+        {
+            Debug.Log("game end");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "missile")
+        {
+            hp -= 0.05f;
+            //health.value -= 5;
+        }
+        else if(other.gameObject.tag == "shell")
+        {
+            //health.value -= 1;
+            hp -= 0.01f;
+
+        }
+
+    }
+
+}
