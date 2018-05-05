f765186a8b8613bece24a5b8fed161f1e51acbab
TJKendall89 <tjkendall@uclan.ac.uk>
2018-05-05 20:35:11 +0100

    Skyboxes
    added skyboxes and added to script to load them

--- Assets/OfflineBattle/LoadLevel.cs
+++ Assets/OfflineBattle/LoadLevel.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class LoadLevel : MonoBehaviour {
 
     public GameObject[] levels;
+    public Material[] skyBoxes;
 
     // Load level before any other script starts
     void Awake () {
         int levelNo = PlayerPrefs.GetInt("Level");
         GameObject go = Instantiate(levels[levelNo]);
+        RenderSettings.skybox = skyBoxes[levelNo];
 	}
 }
