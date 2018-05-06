5cf4b771909efce8de19a831ed60c7b2a06a6097
tstijssen <TSTijssen@DEV.LOCAL>
2018-05-06 09:57:08 +0100

    onlinestuff

--- Assets/Lobby/Scripts/Lobby/LobbyTopPanel.cs
+++ Assets/Lobby/Scripts/Lobby/LobbyTopPanel.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using XInputDotNetPure;
 
 namespace Prototype.NetworkLobby
 {
     public class LobbyTopPanel : MonoBehaviour
     {
         public bool isInGame = false;
-
+        public GameObject lobbyCamera;
         protected bool isDisplayed = true;
         protected Image panelImage;
+        bool canInteract = true;
+        bool lobbyCamActive = true;
 
         void Start()
         {
@@ -19,10 +22,31 @@ namespace Prototype.NetworkLobby
 
         void Update()
         {
-            if (!isInGame)
-                return;
+            lobbyCamera.SetActive(!isInGame);
 
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (!isInGame)
+            {
+                if(!lobbyCamActive)
+                {
+                    this.transform.parent.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceCamera;
+                    this.transform.parent.GetComponent<Canvas>().worldCamera = lobbyCamera.GetComponent<Camera>();
+                    lobbyCamActive = true;
+                }
+
+                return;
+            }
+            else
+            {
+                if(lobbyCamActive)
+                {
+                    this.transform.parent.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+                    lobbyCamActive = false;
+                }
+            }
+
+            GamePadState m_GamePad = GamePad.GetState(PlayerIndex.One);
+
+            if (Input.GetKeyDown(KeyCode.Escape) || (canInteract && m_GamePad.IsConnected && m_GamePad.Buttons.Start == ButtonState.Pressed))
             {
                 ToggleVisibility(!isDisplayed);
             }
@@ -32,6 +56,7 @@ namespace Prototype.NetworkLobby
         public void ToggleVisibility(bool visible)
         {
             isDisplayed = visible;
+
             foreach (Transform t in transform)
             {
                 t.gameObject.SetActive(isDisplayed);
@@ -41,6 +66,15 @@ namespace Prototype.NetworkLobby
             {
                 panelImage.enabled = isDisplayed;
             }
+            canInteract = false;
+            StartCoroutine(ButtonClick());
+        }
+
+        IEnumerator ButtonClick()
+        {
+            Debug.Log("Delaying");
+            yield return new WaitForSeconds(0.25f);
+            canInteract = true;   // After the wait is over, the player can interact with the menu again.
         }
     }
 }
